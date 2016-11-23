33933c7509ecb3f5649573c7e794d370b9cd3b3f
Satoru Makino <autani@gmail.com>
2016-11-24 03:36:22 +0900

    実行時に data/config.xml を読み込んでウィンドウを配置し、設定されたデバイスIDでカメラをOPENします。 Data/config.xml が存在しない場合にはデフォルトの値を使用し、data/config.xml を生成します。 gui window で sキーを押すとウィンドウの位置を data/config.xml に保存します。

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,35 @@
 #include "ofMirror.hpp"
 #include "ofAppGLFWWindow.h"
 
+#include "config.h"
+
 //========================================================================
 int main( ){
-	ofGLFWWindowSettings settings;
-
+    
+    // Read config.xml  --- if not, create.
+    // * default values are Config::Config()
+    config.load();
+    
     //mirror 1
-	settings.width = 500;
-	settings.height = 750;
-	settings.setPosition(ofVec2f(300,0));
-	settings.resizable = true;
-	shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-
+    shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(config.mirror1.settings);
+    
     //GUI
-	settings.width = 640;
-	settings.height = 720;
-	settings.setPosition(ofVec2f(0,0));
-	settings.resizable = false;
-    settings.shareContextWith = mainWindow;
-	shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
+    config.gui.settings.shareContextWith = mainWindow;
+    shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(config.gui.settings);
     
     //mirror 2
-    settings.width = 500;
-    settings.height = 750;
-    settings.setPosition(ofVec2f(300,0));
-    settings.resizable = true;
-    shared_ptr<ofAppBaseWindow> mirrorWindow = ofCreateWindow(settings);
-
-	shared_ptr<ofApp> mainApp(new ofApp);
-	shared_ptr<GuiApp> guiApp(new GuiApp);
+    shared_ptr<ofAppBaseWindow> mirrorWindow = ofCreateWindow(config.mirror2.settings);
+    
+    shared_ptr<ofApp> mainApp(new ofApp);
+    shared_ptr<GuiApp> guiApp(new GuiApp);
     shared_ptr<ofMirror> mirrorApp(new ofMirror);
-	mainApp->gui = guiApp;
+    mainApp->gui = guiApp;
     mirrorApp->gui = guiApp;
-
-
-	ofRunApp(guiWindow, guiApp);
+    
+    
+    ofRunApp(guiWindow, guiApp);
     ofRunApp(mirrorWindow, mirrorApp);
-	ofRunApp(mainWindow, mainApp);
-	ofRunMainLoop();
-
+    ofRunApp(mainWindow, mainApp);
+    ofRunMainLoop();
+    
 }
